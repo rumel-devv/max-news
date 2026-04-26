6b504b9ac120972658f998f9bb5145578c023f9d
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-26 14:38:42 +0600

    added signin & signup page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,11 +1,75 @@
-
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterPage = () => {
-    return (
-        <div>
-           <h2>Register page</h2>
-        </div>
-    );
+  const { register, handleSubmit, formState:{ errors }} = useForm();
+
+  const handleRegister = (data) => {
+        const {name,email,photo,password} = data ;
+  };
+
+  return (
+    <div className="w-9/12 mx-auto min-h-[80vh] flex items-center justify-center bg-gray-100  mt-4 rounded-md">
+      <div className="bg-white p-12 mb-4 rounded-md ">
+        <h2 className="text-xl font-semibold text-center">
+         Register your account
+        </h2>
+        <form onSubmit={handleSubmit(handleRegister)}>
+
+          <fieldset className="fieldset  rounded-box ">
+            <label className="label text-md text-black">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Name"
+              {...register("name",{ required: "Name field is requeird" })}
+            />
+            {errors.name && (<p className="mt-2 text-red-500"> {errors.password.message} </p>)}
+          </fieldset>
+
+          <fieldset className="fieldset  rounded-box ">
+            <label className="label text-md text-black">Photo url</label>
+            <input
+              type="text"
+              name="photo"
+              className="input"
+              placeholder="Photo url"
+              {...register("photo",{ required: "photo url field is requeird" })}
+            />
+            {errors.photo && (<p className="mt-2 text-red-500"> {errors.password.message} </p>)}
+          </fieldset>
+
+          <fieldset className="fieldset  rounded-box ">
+            <label className="label text-md text-black">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+              {...register("email",{ required: "email field is requeird" })}
+            />
+            {errors.email && (<p className="mt-2 text-red-500"> {errors.password.message} </p>)}
+          </fieldset>
+
+          <fieldset className="fieldset  rounded-box ">
+            <label className="label text-md text-black">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+              {...register("password",{ required: "password field is requeird" })}
+            />
+            {errors.password && (<p className="mt-2 text-red-500"> {errors.password.message} </p>)}
+          </fieldset>
+          <button className="btn w-full bg-black text-white mt-2">Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
